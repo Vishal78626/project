c102281460258c563a71269933355e451a8cd241
Vishal78626 <vishalgupta78626@gamil.com>
2021-04-23 19:47:32 +0530

    login page b

--- login.php
+++ login.php
@@ -11,46 +11,38 @@
 		/*		T 		R 	 B 	  L
 		margin:130px 500px 50px 550px;*/
 	
-	/*input{
-		font-size: 10px;
-  		border-style: solid;
-  		border-radius: 2px;
-		}*/
-	
-	/*.input_box{
-		background-color: #E9D9D5;
-		font-style: italic;
-	}*/
-	
-	.form-control{
-		font-size: 25px;
-		padding: 10px;
-	}
-	.form-label{
-		margin-bottom: 0px;
-	}
-	.login_button{
-		margin-left: 33%;
-		padding: 3px;
-		background-color: #f08f6b;}
-
-	button{
-		margin-left: 28%;
-		margin-right: auto;
-		padding: 3px;
-		background-color: #f08f6b;
-		font-size: 30px;
-		border-radius: 10px;
-		border-style: solid;
-	}
-	
-	body{
-		background-image: linear-gradient(to right, coral,#C0C0C0);
+		.form-control{
+			font-size: 20px;
+			padding: 10px;
+		}
+		
+		.form-label{
+			margin-bottom: 0px;
+		}
+		
+		.login_button{
+			margin-left: 37%;
+			padding: 3px;
+			background-color: #f08f6b;
+			font-size:25px;
 		}
 
-	.check_box{
-		padding-left: 10px;
-	}
+		button{
+			margin-left: 33%;
+			padding: 3px;
+			background-color: #f08f6b;
+			font-size: 25px;
+			border-radius: 10px;
+			border-style: solid;
+		}
+		
+		body{
+			background-image: linear-gradient(to right, coral,#C0C0C0);
+			}
+
+		.check_box{
+			padding-left: 10px;
+		}
 
 	</style>
 
@@ -58,65 +50,62 @@
 <body>
 
 <!--logindetail.php-->
+<section class="container-fluid">
+	<section class="row justify-content-center align-items-center">
+		<section class="col-12 col-sm-12 col-md-12 col-lg-12">
+			<form action="" method="POST" name="login">
 
-<form action="" method="POST" name="login">
-<div class="container">
+			    <div class="mb-0">
+		        	<h3 class="text-center" style=color:coral;font-style:italic;font-weight:bold;>Login Here</h3>
+		        </div>
 
-<div class="mb-0">
-<label class="form-label">&nbsp;Username : </label>
-<input class="form-control" type="email" name="username" placeholder="abc@gmail.com" required>
-</div>
+				<div class="mb-0">
+					<label class="form-label">&nbsp;Username : </label>
+					<input class="form-control" type="email" name="username" placeholder="abc@gmail.com" required>
+				</div>
 
-<div class="mb-0">
-<label class="form-label">&nbsp;Password : </label>
-<input class="form-control" type="password" id="show" name="password" placeholder="password" required>
-<p class="check_box"><input type="checkbox" onclick="show_pass()"><span style="font-size: 20px;font-style: italic;"> Show Password</span></p>
-</div>
+				<div class="mb-0">
+					<label class="form-label">&nbsp;Password : </label>
+					<input class="form-control" type="password" id="show" name="password" placeholder="password" required>
+					<p class="check_box"><input type="checkbox" onclick="show_pass()"><span style="font-size: 20px;font-style: italic;"> Show Password</span></p>
+				</div>
 
-<div class="mb-0">
-<p><button class="login_button" type="submit" name="s">Login</button></p>
-</div>
-</form>
+				<div class="mb-0">
+					<p><button class="login_button align-items-center" type="submit" name="s">Login</button></p>
+				</div>
+			</form>
 
+			<form action="register.php" method="POST">
+				<div class="mb-0">
+					<p style="text-align:center;font-size: 20px;font-weight: bold;font-style: italic;">New User? Register First</p>
+					<p><button>Register</button></p>
+				</div>
+			</form>
+		</section>	
+	</section>
 
-<form action="register.php" method="POST">
-<div class="mb-0">
-<p style="text-align:center;font-size: 20px;font-weight: bold;font-style: italic;">New User? Register First</p>
-<p><button>Register</button></p>
-</div>
-</form>
-
-
-<?php
-if(isset($_POST['s']))
-{
-	$username=$_POST['username'];
-	$password=$_POST['password'];
-	include('connect.php');
-	session_start();
-	$qry="select firstname,lastname from register where email='$username' and password='$password'";
-	$result=mysqli_query($con,$qry);
-	//$count=mysqli_num_rows($result);
-	$i=0;
-	while($r=mysqli_fetch_array($result)) {
-		$_SESSION["user_name"]=$r['firstname'];
-		$_SESSION["last_name"]=$r['lastname'];
-		header('Location:logindetail.php');
-		//echo "<br>Username : ".$r['firstname'];
-		$i++;
+	<?php
+	if(isset($_POST['s']))
+	{
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+		include('connect.php');
+		session_start();
+		$qry="select firstname,lastname from register where email='$username' and password='$password'";
+		$result=mysqli_query($con,$qry);
+		//$count=mysqli_num_rows($result);
+		$i=0;
+		while($r=mysqli_fetch_array($result))
+		 {
+			$_SESSION["user_name"]=$r['firstname'];
+			$_SESSION["last_name"]=$r['lastname'];
+			header('Location:logindetail.php');
+			$i++;
+		}
+		echo "<p style=color:red;text-align:center;font-size:20px;font-style:italic;>Login unsuccessful</p>";
 	}
-	//if ($count>0) {
-		//$_SESSION["user_name"]=$username;
-		//header('Location:logindetail.php');
-		//echo "<br>Login succesful";	
-	//}
-	//if(!$count>0){
-		echo "<p style=color:red;text-align:center;font-size:20px;>Login unsuccessful</p>";
-	//	}
-	}
-?>
-
-</div>
+	?>
+</section>
 
 <script type="text/javascript">
 	function show_pass(){
@@ -129,6 +118,8 @@ if(isset($_POST['s']))
 	}
 	}
 </script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
 </body>
 </html> 
--- register.php
+++ register.php
@@ -8,7 +8,7 @@
     <style type="text/css">
             
         .form-control{
-        font-size: 25px;
+        font-size: 20px;
         padding: 10px;
         }
                 
@@ -21,28 +21,33 @@
         }
 
         button{
-        margin-left: 28%;
+        margin-left: 32%;
         margin-right: auto;
         padding: 3px;
         background-color: #f08f6b;
-        font-size: 30px;
+        font-size: 25px;
         border-radius: 10px;
         border-style: solid;
         }
 
         .login_button{
-        margin-left: 33%;
+        margin-left: 37%;
         padding: 3px;
         background-color: #f08f6b;
+        font-size:25px;
         }
 
     </style>
 
 </head>
-    <body bgcolor="skyblue">
+    <body>
     	
     <form method="POST" name="register">
     <div class="container">
+
+        <div class="mb-3">
+            <label class="form-label" style=color:coral;margin-left:28%;font-weight:bold;>Register Here</label>
+        </div>
         
         <div class="mb-0">
             <label class="form-label">&nbsp;Firstname : </label>
@@ -59,21 +64,21 @@
             <input class="form-control" type='email' name='email' id='email' placeholder="eg : abc@gmail.com" required>
         </div>
 
-        <div class="mb-2">
+        <div class="mb-4">
             <label class="form-label">&nbsp;Password : </label> 
             <input class="form-control" type='password' name='password' placeholder='Password' required>
         </div>
 
-        <div class="mb-2">
-            <button type="submit" name="s">Register</button>
+        <div class="mb-4">
+            <span><button type="submit" name="s">Register</button>
             <!--<input type='submit' name='s' value='Register now'>-->
         </div>
     </form>
     
 
     <form method="POST" action="login.php">
-        <div class="mb-2">
-            <button class="login_button" type="submit">Login</button>
+        <div class="mb-4">
+            <button class="login_button" type="submit">Login</button></span>
             <!--<a href="login.php"><input type="submit" value="Login"></a>-->
         </div>
     </form>
@@ -92,10 +97,10 @@
         	$result = mysqli_query($con,$qry);
         	if($result)
         	{
-                echo"<p style=color:green;text-align:center;font-size:20px;>You are succesfully Registered with WE-SHARE</p>";        		
+                echo"<p style=color:green;text-align:center;font-size:20px;font-style:italic;>You are succesfully Registered with WE-SHARE</p>";        		
         	}
             else{
-                echo"<p style=color:red;text-align:center;font-size:20px;>Not Registered,Use another email address</p>";
+                echo"<p style=color:red;text-align:center;font-size:20px;font-style:italic;>Not Registered,Use another email address</p>";
             }
         }
         ?>
