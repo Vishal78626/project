c8dbec623e270644d32d6373400b839a73174b89
Vishal78626 <vishalgupta78626@gamil.com>
2021-05-14 15:37:43 +0530

    edit

--- login.php
+++ login.php
@@ -1,116 +1,94 @@
-<!doctype html>
+<!--
+<?php
+	//include('connect.php');
+	//session_start();
+?>
+-->
+<!DOCTYPE html>
 <html lang="en">
- <head>
-
+<head>
  	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
 
     <link rel="stylesheet" type="text/css" href="bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
 	<style type="text/css">
 		/*		T 		R 	 B 	  L
 		margin:130px 500px 50px 550px;*/
-	
-		.form-control{
-			font-size: 20px;
-			padding: 10px;
-		}
-		
-		.form-label{
-			margin-bottom: 0px;
-		}
-		
-		.login_button{
-			margin-left: 37%;
-			padding: 3px;
-			background-color: #f08f6b;
-			font-size:25px;
-		}
-
-		button{
-			margin-left: 33%;
-			padding: 3px;
-			background-color: #f08f6b;
-			font-size: 25px;
-			border-radius: 10px;
-			border-style: solid;
-		}
-		
-		body{
-			background-image: linear-gradient(to right, coral,#C0C0C0);
-			}
-
-		.check_box{
-			padding-left: 10px;
-		}
-
-		/*.container{
-			margin-top: 10%;
-			margin-bottom: 10%;
-			display: block;
-		}*/
-
+	.centerdiv{
+		width: 400px;
+		height: 540px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		background-color: #fbf0f0;
+		padding: 10px;
+		border:5px solid #f08f6b;
+		border-radius: 10px;
+		box-shadow:0px 0px 20px 2px;
+	}
 	</style>
 
 </head>
 <body>
 
 <!--logindetail.php-->
-<section class="container-fluid">
-	<section class="row justify-content-center align-items-center">
-		<section class="col-12 col-sm-4 col-md-8 col-lg-12">
-			<form action="" method="POST" name="login">
 
-			    <div class="mb-0">
-		        	<h3 class="text-center" style=color:coral;font-style:italic;font-weight:bold;>Login Here</h3>
-		        </div>
+<section class="bgimg">
+	<section class="centerdiv">
+		<form action="" method="POST" name="login">
+			<div class="mb-0">
+		      	<h2 class="text-center" style=color:coral;font-weight:bold;>Login Here</h2>
+		    </div>
 
-				<div class="mb-0">
-					<label class="form-label">&nbsp;Username : </label>
-					<input class="form-control" type="email" name="username" placeholder="abc@gmail.com" required>
-				</div>
+		    <div class="mb-0">
+				<label class="form-label">&nbsp;Username</label>
+				<input class="form-control" type="email" name="username" placeholder="abc@gmail.com" required>
+			</div>
 
-				<div class="mb-0">
-					<label class="form-label">&nbsp;Password : </label>
-					<input class="form-control" type="password" id="show" name="password" placeholder="password" required>
-					<p class="check_box"><input type="checkbox" onclick="show_pass()"><span style="font-size: 20px;font-style: italic;"> Show Password</span></p>
-				</div>
+			<div class="mb-0">
+				<label class="form-label">&nbsp;Password</label>
+				<input class="form-control" type="password" id="show" name="password" placeholder="password" required>
+				<p class="check_box"><input type="checkbox" onclick="show_pass()"><span style="font-size: 20px;font-style: italic;"> Show Password</span></p>
+			</div>
 
-				<div class="mb-0">
-					<p><button class="login_button align-items-center" type="submit" name="s">Login</button></p>
-				</div>
-			</form>
+			<div class="mb-0 text-center">
+				<p><button class="login_button align-items-center" type="submit" name="s">Login</button></p>
+			</div>
+		</form>
 
-			<form action="register.php" method="POST">
-				<div class="mb-0">
-					<p style="text-align:center;font-size: 20px;font-weight: bold;font-style: italic;">New User? Register First</p>
-					<p><button>Register</button></p>
-				</div>
-			</form>
-		</section>	
-	</section>
+		<form action="register.php" method="POST">
+			<div class="mb-0 text-center">
+				<p style="text-align:center;font-size:25px;font-weight:bold;font-style: italic;">New User? Register First</p>
+				<p><button>Register</button></p>
+			</div>
+		</form>
 
-	<?php
-	if(isset($_POST['s']))
+<?php
+if(isset($_POST['s']))
+{
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	include('connect.php');
+	session_start();
+	$qry="select firstname,lastname from register where email='$username' and password='$password'";
+	$result=mysqli_query($con,$qry);
+	//$count=mysqli_num_rows($result);
+	$i=0;
+	while($r=mysqli_fetch_array($result))
 	{
-		$username=$_POST['username'];
-		$password=$_POST['password'];
-		include('connect.php');
-		session_start();
-		$qry="select firstname,lastname from register where email='$username' and password='$password'";
-		$result=mysqli_query($con,$qry);
-		//$count=mysqli_num_rows($result);
-		$i=0;
-		while($r=mysqli_fetch_array($result))
-		 {
-			$_SESSION["user_name"]=$r['firstname'];
-			$_SESSION["last_name"]=$r['lastname'];
-			header('Location:logindetail.php');
-			$i++;
-		}
-		echo "<p style=color:red;text-align:center;font-size:20px;font-style:italic;>Login unsuccessful</p>";
+		$_SESSION["user_name"]=$r['firstname'];
+		$_SESSION["last_name"]=$r['lastname'];
+		header('Location:logindetail.php');
+		$i++;
 	}
-	?>
+	echo "<p style=color:red;text-align:center;font-size:25px;font-style:italic;>Login unsuccessful</p>";
+}
+?>
+	</section>
 </section>
 
 <script type="text/javascript">
@@ -129,3 +107,59 @@
 
 </body>
 </html> 
+
+<!--
+<section class="container form-container">
+	<section class="row justify-content-center">
+		<section class="col-12 col-lg-12">
+			<form action="" method="POST" name="login">
+
+			    <div class="mb-0">
+		        	<h2 class="text-center" style=color:coral;font-style:italic;>Login Here</h2>
+		        </div>
+
+				<div class="mb-0">
+					<label class="form-label">&nbsp;Username</label>
+					<input class="form-control" type="email" name="username" placeholder="abc@gmail.com" required>
+				</div>
+
+				<div class="mb-0">
+					<label class="form-label">&nbsp;Password</label>
+					<input class="form-control" type="password" id="show" name="password" placeholder="password" required>
+					<p class="check_box"><input type="checkbox" onclick="show_pass()"><span style="font-size: 20px;font-style: italic;"> Show Password</span></p>
+				</div>
+
+				<div class="mb-0 text-center">
+					<p><button class="login_button" type="submit" name="s">Login</button></p>
+				</div>
+
+			</form>
+
+			<form action="register.php" method="POST">
+				<div class="mb-0 text-center">
+					<p style="text-align:center;font-size:25px;font-weight:bold;font-style: italic;">New User? Register First</p>
+					<p><button>Register</button></p>
+				</div>
+			</form>-->
+		<!--</section>	
+	</section>-->
+<!--
+			/*body{
+			background-image: linear-gradient(to right, coral,#C0C0C0);
+			font-style:italic;
+			font-size: 25px;
+			font-family: 'Crimson','sans-serif';
+			margin: 0px;
+			padding: 0px;
+			}*/
+
+		/*.form-container{
+			display: block;
+						width: 30%;
+			background-color: #fbf0f0;
+			padding: 10px;
+  			border:5px solid #f08f6b;
+  			border-radius: 20px;
+  			margin-top: 5%; 
+  			box-shadow:0px 0px 20px 2px;
+		}*/-->
--- register.php
+++ register.php
@@ -1,87 +1,73 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
+
 <link rel="stylesheet" type="text/css" href="bootstrap.css">
+<link rel="stylesheet" type="text/css" href="style.css">
     <style type="text/css">
-            
-        .form-control{
-        font-size: 20px;
-        padding: 10px;
-        }
-                
-        .form-label{
-        margin-bottom: 0px;
-        }
-
-        body{
-        background-image: linear-gradient(to right, coral,#C0C0C0);
-        }
-
-        button{
-        margin-left: 32%;
-        margin-right: auto;
-        padding: 3px;
-        background-color: #f08f6b;
-        font-size: 25px;
-        border-radius: 10px;
-        border-style: solid;
-        }
-
-        .login_button{
-        margin-left: 37%;
-        padding: 3px;
-        background-color: #f08f6b;
-        font-size:25px;
+        .centerdiv{
+            width: 400px;
+            height: 650px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: #fbf0f0;
+            padding: 10px;
+            border:5px solid #f08f6b;
+            border-radius: 10px;
+            box-shadow:0px 0px 20px 2px;
         }
 
     </style>
 
 </head>
-    <body>
-    	
-    <form method="POST" name="register">
-    <div class="container">
-
-        <div class="mb-3">
-            <label class="form-label" style=color:coral;margin-left:28%;font-weight:bold;>Register Here</label>
-        </div>
+<body>
+ 
+ <div class="bgimg">
+    <div class="centerdiv">   	
+        <form method="POST" name="register">
         
-        <div class="mb-0">
-            <label class="form-label">&nbsp;Firstname : </label>
-            <input class="form-control" type='text' name='fname' placeholder='eg : john' required>
-        </div>
 
-        <div class="mb-0">
-            <label class="form-label">&nbsp;Lastname : </label>
-            <input class="form-control" type='text' name='lname' placeholder='eg : surname' required>
-        </div>
-  
-        <div class="mb-0">
-            <label class="form-label">&nbsp;Email : </label>
-            <input class="form-control" type='email' name='email' id='email' placeholder="eg : abc@gmail.com" required>
-        </div>
+            <div class="mb-0 text-center">
+                <label class="form-label" style=color:coral;font-weight:bold;>Register Here</label>
+            </div>
+            
+            <div class="mb-0">
+                <label class="form-label">&nbsp;Firstname : </label>
+                <input class="form-control" type='text' name='fname' placeholder='eg : john' required>
+            </div>
 
-        <div class="mb-4">
-            <label class="form-label">&nbsp;Password : </label> 
-            <input class="form-control" type='password' name='password' placeholder='Password' required>
-        </div>
+            <div class="mb-0">
+                <label class="form-label">&nbsp;Lastname : </label>
+                <input class="form-control" type='text' name='lname' placeholder='eg : surname' required>
+            </div>
+      
+            <div class="mb-0">
+                <label class="form-label">&nbsp;Email : </label>
+                <input class="form-control" type='email' name='email' id='email' placeholder="eg : abc@gmail.com" required>
+            </div>
 
-        <div class="mb-4">
-            <span><button type="submit" name="s">Register</button>
-            <!--<input type='submit' name='s' value='Register now'>-->
-        </div>
-    </form>
+            <div class="mb-2">
+                <label class="form-label">&nbsp;Password : </label> 
+                <input class="form-control" type='password' name='password' placeholder='Password' required>
+            </div>
+
+            <div class="mb-2 text-center">
+                <button type="submit" name="s">Register</button>
+            </div>
+        </form>
     
 
-    <form method="POST" action="login.php">
-        <div class="mb-4">
-            <button class="login_button" type="submit">Login</button></span>
-            <!--<a href="login.php"><input type="submit" value="Login"></a>-->
-        </div>
-    </form>
+        <form method="POST" action="login.php">
+            <div class="mb-2 text-center">
+                <button class="login_button" type="submit">Login</button>
+            </div>
+        </form>
     
 
         <?php
@@ -105,6 +91,9 @@
         }
         ?>
     </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     
-    </body>
+</body>
 </html>
